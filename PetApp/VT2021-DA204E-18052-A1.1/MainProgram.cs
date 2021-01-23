20d13e13ae7d1523814592143763529dbd450984
Lena Tevar <13490448+LenaTevar@users.noreply.github.com>
2021-01-23 15:12:55 +0100

    first assignment

---
+++ PetApp/VT2021-DA204E-18052-A1.1/MainProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VT2021_DA204E_18052_A1._1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Pet myPet = new Pet();
+            myPet.GetInput();
+            myPet.ToConsole();
+        }
+    }
+}
---
+++ PetApp/VT2021-DA204E-18052-A1.1/boolean.cs
@@ -0,0 +1,6 @@
+﻿namespace VT2021_DA204E_18052_A1._1
+{
+    internal class boolean
+    {
+    }
+}
